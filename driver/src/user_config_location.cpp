b03ab205fb75ed6c1ce6972514e53fea7f5d0053
Konstantin Litvinenko <to.darkangel@gmail.com>
2016-03-19 20:13:22 +0200

    Added ability to specify user-config.ham location using environment

--- driver/src/user_config_location.cpp
+++ driver/src/user_config_location.cpp
@@ -4,25 +4,21 @@
 
 hammer::location_t get_user_config_location()
 {
+   const char* user_provided_user_config_path = getenv("HAMMER_USER_CONFIG");
+   if (user_provided_user_config_path)
+      return hammer::location_t(user_provided_user_config_path);
+
 #if defined(_WIN32)
    const char* home_path = getenv("USERPROFILE");
+#elif defined(__linux__)
+      const char* home_path = getenv("HOME");
+#else
+#     error "Platform not supported"
+#endif
+
    if (home_path != NULL)
       return hammer::location_t(home_path) / "user-config.ham";
    else   
       throw std::runtime_error("Can't find user home directory.");
-
-#else
-#   if defined(__linux__)
-   
-   const char* home_path = getenv("HOME");
-   if (home_path != NULL)
-      return hammer::location_t(home_path) / "user-config.ham";
-   else
-      throw std::runtime_error("Can't find user home directory.");
-   
-#   else
-#      error "Platform not supported"
-#   endif
-#endif
 }
 

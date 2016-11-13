f8a4b0611d8d5f07a386d5843ef7fe1058453178
Konstantin Litvinenko <to.darkangel@gmail.com>
2016-11-13 18:37:19 +0200

    Fix compile-fail behaviour - we didn't respect propagated sources from test-suit target

--- core/include/hammer/core/testing_compile_fail_meta_target.h
+++ core/include/hammer/core/testing_compile_fail_meta_target.h
@@ -8,10 +8,16 @@ namespace hammer {
 class testing_compile_fail_meta_target : public typed_meta_target
 {
    public:
+      typedef typed_meta_target base;
       testing_compile_fail_meta_target(hammer::project* p,
                                        const pstring& name,
                                        const requirements_decl& req,
                                        const requirements_decl& usage_req);
+   protected:
+      void instantiate_impl(const main_target* owner,
+                            const feature_set& build_request,
+                            std::vector<basic_target*>* result,
+                            feature_set* usage_requirements) const override;
 };
 
 }
--- core/src/testing_compile_fail_meta_target.cpp
+++ core/src/testing_compile_fail_meta_target.cpp
@@ -1,6 +1,8 @@
 #include <hammer/core/testing_compile_fail_meta_target.h>
 #include <hammer/core/engine.h>
 #include <hammer/core/types.h>
+#include <hammer/core/feature_set.h>
+#include <hammer/core/feature.h>
 #include <hammer/core/testing_main_target.h>
 
 namespace hammer {
@@ -11,6 +13,28 @@ testing_compile_fail_meta_target::testing_compile_fail_meta_target(project* p,
                                                                    const requirements_decl& usage_req)
    : typed_meta_target(p, name, req, usage_req, p->get_engine()->get_type_registry().get(types::TESTING_COMPILE_FAIL))
 {
+}
+
+void testing_compile_fail_meta_target::instantiate_impl(const main_target* owner,
+                                                        const feature_set& build_request,
+                                                        std::vector<basic_target*>* result,
+                                                        feature_set* usage_requirements) const
+{
+   // we need to convert <testing.additional-source> into <use> in build request
+   // because that is how test-suite target will pass sources down if any
+   feature_set* new_build_request = get_engine()->feature_registry().make_set();
+   for (const feature* f : build_request) {
+      if (f->name() == "testing.additional-source") {
+         feature* source = get_engine()->feature_registry().create_feature("use", "");
+         source->get_dependency_data() = f->get_dependency_data();
+         new_build_request->join(source);
+      } else {
+         feature* f_copy = get_engine()->feature_registry().clone_feature(*f);
+         new_build_request->join(f_copy);
+      }
+   }
+
+   base::instantiate_impl(owner, *new_build_request, result, usage_requirements);
 }
 
 }

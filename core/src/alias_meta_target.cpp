a80437740f4d0932a08084b98680ad32026968e6
Konstantin Litvinenko <to.darkangel@gmail.com>
2016-12-28 18:37:58 +0200

    adjust only alias sources, not all that come from usage requirements

--- core/src/alias_meta_target.cpp
+++ core/src/alias_meta_target.cpp
@@ -29,6 +29,7 @@ void alias_meta_target::instantiate_impl(const main_target* owner,
    if (owner != NULL) {
       this->usage_requirements().eval(owner->properties(), usage_requirements);
 
+      feature_set& sources_as_features = *get_engine()->feature_registry().make_set();
       for (const source_decl& sd : sources()) {
          feature* f = get_engine()->feature_registry().create_feature("source", "");
          source_decl new_sd = sd;
@@ -41,10 +42,11 @@ void alias_meta_target::instantiate_impl(const main_target* owner,
 
          f->set_dependency_data(new_sd, this);
 
-         usage_requirements->join(f);
+         sources_as_features.join(f);
       }
 
-      adjust_dependency_features_sources(*usage_requirements, *this, owner);
+      adjust_dependency_features_sources(sources_as_features, *this, owner);
+      usage_requirements->join(sources_as_features);
    } else {
       // top level alias instantiation
       vector<basic_target*> sources;

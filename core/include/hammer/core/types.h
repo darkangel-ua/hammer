ad2e8b774435daa6acd22b7287d2934bc16c00d9
Konstantin Litvinenko <to.darkangel@gmail.com>
2018-03-25 11:23:21 +0300

    formatting and c++11

--- core/include/hammer/core/types.h
+++ core/include/hammer/core/types.h
@@ -1,40 +1,41 @@
-#if !defined(h_883d3846_ffd7_4a31_840e_13148216c79b)
-#define h_883d3846_ffd7_4a31_840e_13148216c79b
+#pragma once
 
-namespace hammer
-{
-   class type_registry;
-   class feature_registry;
-   class type_tag;
+namespace hammer {
 
-   namespace types
-   {
-      extern const type_tag CPP;
-      extern const type_tag C;
-      extern const type_tag H;
-      extern const type_tag RC;
-      extern const type_tag RES;
-      extern const type_tag OBJ;
-      extern const type_tag PCH;
-      extern const type_tag LIB;
-      extern const type_tag SHARED_LIB;
-      extern const type_tag PREBUILT_SHARED_LIB;
-      extern const type_tag IMPORT_LIB;
-      extern const type_tag STATIC_LIB;
-      extern const type_tag PREBUILT_STATIC_LIB;
-      extern const type_tag SEARCHED_LIB;
-      extern const type_tag SEARCHED_SHARED_LIB;
-      extern const type_tag SEARCHED_STATIC_LIB;
-      extern const type_tag HEADER_LIB;
-      extern const type_tag EXE;
-      extern const type_tag UNKNOWN;
-      extern const type_tag COPIED;
-      extern const type_tag TESTING_OUTPUT;
-      extern const type_tag TESTING_RUN_PASSED;
-      extern const type_tag TESTING_COMPILE_FAIL;
+class type_registry;
+class feature_registry;
+class type_tag;
 
-      void register_standart_types(type_registry& tr, feature_registry& fr);
-   }
 }
 
-#endif //h_883d3846_ffd7_4a31_840e_13148216c79b
+namespace hammer { namespace types {
+
+extern const type_tag CPP;
+extern const type_tag C;
+extern const type_tag H;
+extern const type_tag RC;
+extern const type_tag RES;
+extern const type_tag OBJ;
+extern const type_tag PCH;
+extern const type_tag LIB;
+extern const type_tag SHARED_LIB;
+extern const type_tag PREBUILT_SHARED_LIB;
+extern const type_tag IMPORT_LIB;
+extern const type_tag STATIC_LIB;
+extern const type_tag PREBUILT_STATIC_LIB;
+extern const type_tag SEARCHED_LIB;
+extern const type_tag SEARCHED_SHARED_LIB;
+extern const type_tag SEARCHED_STATIC_LIB;
+extern const type_tag HEADER_LIB;
+extern const type_tag EXE;
+extern const type_tag UNKNOWN;
+extern const type_tag COPIED;
+extern const type_tag TESTING_OUTPUT;
+extern const type_tag TESTING_RUN_PASSED;
+extern const type_tag TESTING_COMPILE_FAIL;
+
+void register_standart_types(type_registry& tr,
+                             feature_registry& fr);
+
+}}
+
--- core/src/types.cpp
+++ core/src/types.cpp
@@ -1,13 +1,10 @@
-#include "stdafx.h"
+#include <vector>
 #include <hammer/core/types.h>
 #include <hammer/core/target_type.h>
-#include <boost/assign/list_of.hpp>
-#include <vector>
 #include <hammer/core/type_registry.h>
 #include <hammer/core/feature_set.h>
 
 using namespace std;
-using namespace boost::assign;
 
 namespace hammer{ namespace types{
 
@@ -35,30 +32,41 @@ const type_tag TESTING_OUTPUT("TESTING_OUTPUT");
 const type_tag TESTING_RUN_PASSED("TESTING_RUN_PASSED");
 const type_tag TESTING_COMPILE_FAIL("TESTING_COMPILE_FAIL");
 
-void register_standart_types(type_registry& tr, feature_registry& fr)
+void register_standart_types(type_registry& tr,
+                             feature_registry& fr)
 {
-   target_type::suffixes_t cpp_suffixes = list_of(".cpp")(".cc");
+   target_type::suffixes_t cpp_suffixes = {".cpp", ".cc"};
    tr.insert(target_type(CPP, cpp_suffixes));
 
    tr.insert(target_type(C, ".c"));
 
-   target_type::suffixes_t h_suffixes = list_of(".h")(".hpp")(".H");
+   target_type::suffixes_t h_suffixes = {".h", ".hpp", ".H"};
    tr.insert(target_type(H, h_suffixes));
 
    tr.insert(target_type(RC, ".rc"));
    tr.insert(target_type(RES, ".res"));
 
-   target_type::suffixes_t obj_suffixes = list_of<target_type::suffix_def>(".o", *parse_simple_set("<target-os>linux", fr))
-                                                                          (".obj", *parse_simple_set("<target-os>windows", fr));
+   target_type::suffixes_t obj_suffixes = {
+      { ".o", *parse_simple_set("<target-os>linux", fr)},
+      {".obj", *parse_simple_set("<target-os>windows", fr)}
+   };
+
    tr.insert(target_type(OBJ, obj_suffixes));
    tr.insert(target_type(PCH, ".pch"));
    const target_type& tLIB = tr.insert(target_type(LIB, ""));
-   target_type::suffixes_t shared_suffixes = list_of<target_type::suffix_def>(".dll", *parse_simple_set("<target-os>windows", fr))
-                                                                             (".so", *parse_simple_set("<target-os>linux", fr));
+
+   target_type::suffixes_t shared_suffixes = {
+      {".dll", *parse_simple_set("<target-os>windows", fr)},
+      {".so", *parse_simple_set("<target-os>linux", fr)}
+   };
+
    const target_type& tSHARED_LIB = tr.insert(target_type(SHARED_LIB, shared_suffixes, tLIB));
    tr.insert(target_type(PREBUILT_SHARED_LIB, "", tSHARED_LIB));
-   target_type::suffixes_t static_lib_suffixes = list_of<target_type::suffix_def>(".lib", *parse_simple_set("<target-os>windows", fr))
-                                                                                 (".a", *parse_simple_set("<target-os>linux", fr));
+   target_type::suffixes_t static_lib_suffixes = {
+      {".lib", *parse_simple_set("<target-os>windows", fr)},
+      {".a", *parse_simple_set("<target-os>linux", fr)}
+   };
+
    const target_type& tSTATIC_LIB = tr.insert(target_type(STATIC_LIB, static_lib_suffixes, tLIB));
    tr.insert(target_type(PREBUILT_STATIC_LIB, "", tSTATIC_LIB));
 
@@ -69,8 +77,11 @@ void register_standart_types(type_registry& tr, feature_registry& fr)
    tr.insert(target_type(HEADER_LIB, target_type::suffixes_t(), tLIB));
 
 
-   target_type::suffixes_t exe_suffixes = list_of<target_type::suffix_def>(".exe", *parse_simple_set("<target-os>windows", fr))
-                                                                           ("", *parse_simple_set("<target-os>linux", fr));
+   target_type::suffixes_t exe_suffixes = {
+      {".exe", *parse_simple_set("<target-os>windows", fr)},
+      {"", *parse_simple_set("<target-os>linux", fr)}
+   };
+
    tr.insert(target_type(EXE, exe_suffixes));
    tr.insert(target_type(UNKNOWN, ""));
    tr.insert(target_type(COPIED, ""));

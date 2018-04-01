356bcf709f16c90556102ad572609822fe8b3035
Konstantin Litvinenko <to.darkangel@gmail.com>
2018-04-01 10:55:11 +0300

    msvc build fixes

--- parscore/include/hammer/parscore/identifier.h
+++ parscore/include/hammer/parscore/identifier.h
@@ -7,7 +7,15 @@ namespace hammer { namespace parscore {
 
 class identifier : public token {
    public:
-      using token::token;
+      // all these contructor can be replaced by using token::token but msvc-12.0 can't handle this
+      identifier(source_location lok,
+                 unsigned int length)
+         : token(lok, length)
+      {}
+
+      identifier(const char* v) : token(v) {}
+      identifier(const ANTLR3_COMMON_TOKEN_struct* v) : token(v) {}
+      identifier() {}
 };
 
 }}

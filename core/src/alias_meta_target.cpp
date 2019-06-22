af42df7febf5b16be761187a1405e1522d4841da
Konstantin Litvinenko <to.darkangel@gmail.com>
2019-06-22 09:53:35 +0300

    Fix project depedencies and alias interraction

--- core/src/alias_meta_target.cpp
+++ core/src/alias_meta_target.cpp
@@ -33,7 +33,11 @@ void alias_meta_target::instantiate_impl(instantiation_context& ctx,
       this->usage_requirements().eval(build_request, usage_requirements);
 
       feature_set& sources_as_features = *get_engine().feature_registry().make_set();
-      for (const source_decl& sd : sources()) {
+
+      auto sources = this->sources();
+      apply_project_dependencies(sources, *this);
+
+      for (const source_decl& sd : sources) {
          feature* f = get_engine().feature_registry().create_feature("source", "");
          source_decl new_sd = sd;
 
@@ -55,12 +59,13 @@ void alias_meta_target::instantiate_impl(instantiation_context& ctx,
       usage_requirements->join(sources_as_features);
    } else {
       // top level alias instantiation
-      vector<basic_target*> sources;
+      auto sources = this->sources();
+      apply_project_dependencies(sources, *this);
+
       sources_decl simple_targets;
       meta_targets_t meta_targets;
-      split_sources(&simple_targets, &meta_targets, this->sources(), build_request);
-      instantiate_meta_targets(ctx, meta_targets, build_request, nullptr, &sources, usage_requirements);
-      result->insert(result->end(), sources.begin(), sources.end());
+      split_sources(&simple_targets, &meta_targets, sources, build_request);
+      instantiate_meta_targets(ctx, meta_targets, build_request, nullptr, result, usage_requirements);
    }
 }
 
